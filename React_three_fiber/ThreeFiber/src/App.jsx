ded026b9b2d8ce240be4de7042897db5086fb3b9
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-01 23:13:27 +0500

    initiated ThreeJs project in React using React Three Fiber

--- React_three_fiber/ThreeFiber/src/App.jsx
+++ React_three_fiber/ThreeFiber/src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import {Canvas}  from "@react-three/fiber"
+import React, { useRef } from 'react'
+import {Canvas, useFrame}  from "@react-three/fiber"
 import {OrbitControls} from "@react-three/drei"
+
+const RotatinCube = ()=>{
+
+    const meshRef = useRef()
+
+    useFrame(()=>{
+      if(meshRef.current){
+        meshRef.current.rotation.y +=0.01
+        meshRef.current.rotation.x +=0.01
+      }
+    })
+  return (
+    <mesh ref={meshRef}>
+        <cylinderGeometry args={[1,1,1]}/>
+        <meshLambertMaterial color="#468585" emissive="#468585"/>
+    </mesh>
+  )
+}
+
 function App() {
   return (
    <Canvas style={{display:"flex", justifyContent:'center', alignItems:'center', height:"100vh", width:'100vw'}}>
    <OrbitControls enableZoom enablePan enableRotate/>
   <directionalLight position={[1,1,1]} intensity={10} color={0x9CDBA6}/>
   <color attach="background" args={['#F0F0F0']}/>
+  <RotatinCube/>
    </Canvas>
   )
 }
