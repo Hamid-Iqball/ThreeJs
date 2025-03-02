c8135cd944cdfab17f73963a6d5cfd51d16e9f9c
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-02 14:34:15 +0500

    Completed the basic of React_three_fiber

--- React_three_fiber/ThreeFiber/src/App.jsx
+++ React_three_fiber/ThreeFiber/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import {Canvas, useFrame}  from "@react-three/fiber"
-import {OrbitControls} from "@react-three/drei"
+import {OrbitControls, Sparkles} from "@react-three/drei"
 
 const RotatinCube = ()=>{
 
@@ -10,12 +10,14 @@ const RotatinCube = ()=>{
       if(meshRef.current){
         meshRef.current.rotation.y +=0.01
         meshRef.current.rotation.x +=0.01
+        // meshRef.current.rotation.z +=0.01
       }
     })
   return (
     <mesh ref={meshRef}>
         <cylinderGeometry args={[1,1,1]}/>
         <meshLambertMaterial color="#468585" emissive="#468585"/>
+        <Sparkles count={100} scale={1} size={6} speed={0.0002}    color="Orange"/>
     </mesh>
   )
 }
